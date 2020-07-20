b9d962cfa46e8285388e116f8b08e5fd0f56ab39
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-20 01:55:25 -0300

    Updates

---
+++ precache-manifest.beaf82ab6071a94741c04a8da0e68315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d436a0549af8daa9fa6f233513bdd5c1",
+    "url": "/Movie-Cards-Library/index.html"
+  },
+  {
+    "revision": "24ca81173a21a5cedb7f",
+    "url": "/Movie-Cards-Library/static/css/main.dcc329e0.chunk.css"
+  },
+  {
+    "revision": "65152235846ddf4f269e",
+    "url": "/Movie-Cards-Library/static/js/2.b3a1ccff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Cards-Library/static/js/2.b3a1ccff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ca81173a21a5cedb7f",
+    "url": "/Movie-Cards-Library/static/js/main.d7b95018.chunk.js"
+  },
+  {
+    "revision": "d5fcb8337441db50cf6f",
+    "url": "/Movie-Cards-Library/static/js/runtime-main.f7fd75f9.js"
+  }
+]);
